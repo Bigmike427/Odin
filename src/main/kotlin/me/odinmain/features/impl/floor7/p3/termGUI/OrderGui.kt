6b22fdcce64e0f5507e69211eb7ecd88757f56e7
Bigmike427 <willseed07@gmail.com>
2025-03-18 19:26:01 -0400

    no number ever again ever ever again
    numbers=bad
    
    Signed-off-by: Bigmike427 <willseed07@gmail.com>

--- src/main/kotlin/me/odinmain/features/impl/floor7/p3/termGUI/OrderGui.kt
+++ src/main/kotlin/me/odinmain/features/impl/floor7/p3/termGUI/OrderGui.kt
@@ -44,8 +44,6 @@ object OrderGui : TermGui() {
                     box.h.toFloat() * customScale
                 )
             }
-            
-            if (TerminalSolver.showNumbers && index != -1) mcText(amount.toString(), -163 + col * 70 + 26f , -60 + row * 70 + (27f - (textScale * 3) - (gap * 0.5)), 2 + textScale, Color.WHITE)
         }
     }
 }
